15df0b5dceb12262cb5c7ad686e9131412487a6b
Richard Allen Dunks <richarddunks@gmail.com>
2013-10-28 13:03:16 -0400

    changed output file to csv and changed name to be more descriptive

--- getdetails.rb
+++ getdetails.rb
@@ -7,29 +7,25 @@ require 'open-uri'
 idfile = ARGV[0]
 
 c = CSV.open idfile 
+uribase = "http://a841-tfpweb.nyc.gov/bikeshare/get_station_info?point="
 
 c.each do |row|
-	uristring = "http://a841-tfpweb.nyc.gov/bikeshare/get_point_info?point=" + row[0]
+	uristring = uribase + row[0]
 	content=open(uristring).read
 	PARSER = Yajl::Parser.new
 
-	def output_CSV(s)
+  def output_CSV(s)
 
 
+  station_info = [s['id'],s['lat'],s['lon'],s['user_profile_url'],s['reason']].to_csv
+  puts station_info
+  File.open('getdetailsOutput.csv', 'a') do |file|
+    file.puts station_info
+  end
 
-
-	station_info = [s['id'],s['lat'],s['lon'],s['user_profile_url'],s['reason']].to_csv
-	puts station_info
-	File.open('results.txt', 'a') do |file|
-	 file.puts station_info
-	end
-	
 end
 	
-	
-	
-	PARSER.on_parse_complete = method(:output_CSV)
-	PARSER.parse(content)
-	
-	
+  PARSER.on_parse_complete = method(:output_CSV)
+  PARSER.parse(content)
+
 end
